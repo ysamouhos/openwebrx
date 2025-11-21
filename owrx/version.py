5850b858e1082812b6c9cf84cc72a2268780ad88
Yiannis Samouhos <ysamouhos@users.noreply.github.com>
2025-11-22 00:07:39 +0200

    Update version.py

--- owrx/version.py
+++ owrx/version.py
@@ -1,5 +1,5 @@
 from distutils.version import LooseVersion
 
-_versionstring = "1.2.99"
+_versionstring = "1.2.100a"
 looseversion = LooseVersion(_versionstring)
 openwebrx_version = "v{0}".format(looseversion)
